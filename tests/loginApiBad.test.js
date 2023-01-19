20a9c3269718bf2232a85fdc9ddc0c817547562e
Lucas Gomes <lucaslehi@byui.edu>
2023-01-19 11:15:49 -0700

    week 3 add

---
+++ tests/loginApiBad.test.js
@@ -0,0 +1,19 @@
+import fetch from "node-fetch";
+
+it("Should get a login token", async () => {
+  //async allow us to read from the database by moving on and coming back
+
+  const loginResponse = await fetch("https://dev.stedi.me/login", {
+    //await fetch allows it to move on while getting data
+    method: "POST", //method : POST says to give infromation to the server
+    headers: {
+      "Content-Type": "application/text",
+    },
+
+    body: JSON.stringify({
+      userName: "digimon03@yahoo.com",
+      password: "winter2022!",
+    }),
+  });
+  expect(loginResponse.status).toBe(401); //401 means not authorized
+});
